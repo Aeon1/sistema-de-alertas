161f40a419d67efd15f7a278cf943f8ddfe47f46
Aeon1 <joel.r@live.com.mx>
2016-10-07 15:34:27 -0600

    prueba inicio 15

--- sistema de alertas/www/js/my-app.js
+++ sistema de alertas/www/js/my-app.js
@@ -1,14 +1,4 @@
 // Initialize your app
-var myApp = new Framework7();
-
-// Export selectors engine
-var $$ = Dom7;
-
-// Add view
-var mainView = myApp.addView('.view-main', {
-    // Because we use fixed-through navbar we can enable dynamic navbar
-    dynamicNavbar: true
-});
 var myApp = new Framework7({
     init:true,
     preprocess: function (content, url, next) {
@@ -20,4 +10,14 @@ var myApp = new Framework7({
         }
     }
 });
+
+// Export selectors engine
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
+});
+
 
