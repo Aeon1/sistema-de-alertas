48a332ed191d58c8de121d6940a1732d7b13b3a3
Aeon1 <joel.r@live.com.mx>
2016-10-07 15:25:47 -0600

    prueba inicio 14

---
+++ sistema de alertas/www/js/my-app.js
@@ -0,0 +1,23 @@
+// Initialize your app
+var myApp = new Framework7();
+
+// Export selectors engine
+var $$ = Dom7;
+
+// Add view
+var mainView = myApp.addView('.view-main', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
+});
+var myApp = new Framework7({
+    init:true,
+    preprocess: function (content, url, next) {
+        myApp.alert(url);
+        if (url === 'aviso.html') {
+           myApp.alert(url);
+        }else{
+            myApp.alert(url);
+        }
+    }
+});
+
