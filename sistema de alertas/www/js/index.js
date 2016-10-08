834d9f6a25d259ed7bf2d28170aa5520cde1af78
Aeon1 <joel.r@live.com.mx>
2016-10-08 09:38:16 -0600

    prueba de captura nuevo js

--- sistema de alertas/www/js/index.js
+++ sistema de alertas/www/js/index.js
@@ -10,13 +10,26 @@ var mainView = myApp.addView('.view-main', {
     dynamicNavbar: true
 });
 var myApp = new Framework7({
-    init:true,
-    preprocess: function (content, url, next) {
-        myApp.alert(url);
-        if(url === 'aviso.html') {
-           myApp.alert(url);
-        }else{
-            myApp.alert(url);
+    init:true
+});
+function captureSuccess(mediaFiles) {
+        var i, len;
+        for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+            //uploadFile(mediaFiles[i]);
         }
     }
-});
+
+    // Called if something bad happens.
+    //
+    function captureError(error) {
+        var msg = 'An error occurred during capture: ' + error.code;
+        navigator.notification.alert(msg, null, 'Uh oh!');
+    }
+
+    // A button will call this function
+    //
+    function captureImage() {
+        // Launch device camera application,
+        // allowing user to capture up to 2 images
+        navigator.device.capture.captureImage(captureSuccess, captureError, {limit: 2});
+    }
